139eae738b087dd33e465be27469de7823661bf2
Gonzalo Gamez <takosaga@gmail.com>
2023-12-26 15:22:03 +0200

    fix: adjusted stuff in car

--- self_driving_car/car.js
+++ self_driving_car/car.js
@@ -9,7 +9,6 @@ class Car {
         this.acceleration = 0.2;
         this.maxSpeed = 3;
         this.friction = 0.05;
-
         this.angle = 0;
 
         this.sensor = new Sensor(this);
@@ -28,12 +27,14 @@ class Car {
         if (this.controls.reverse) {
             this.speed -= this.acceleration;
         }
+
         if (this.speed > this.maxSpeed) {
             this.speed = this.maxSpeed;
         }
         if (this.speed < -this.maxSpeed / 2) {
             this.speed = -this.maxSpeed / 2;
         }
+
         if (this.speed > 0) {
             this.speed -= this.friction;
         }
@@ -57,6 +58,7 @@ class Car {
         this.x -= Math.sin(this.angle) * this.speed;
         this.y -= Math.cos(this.angle) * this.speed;
     }
+
     draw(ctx) {
         ctx.save();
         ctx.translate(this.x, this.y);
