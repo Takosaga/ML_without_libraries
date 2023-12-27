4d8616ad3936a6381bdfbe9d8dd04b6acb7d6534
Gonzalo Gamez <takosaga@gmail.com>
2023-12-27 18:16:02 +0200

    feat/fix: connected network and fixed network

--- self_driving_car/network.js
+++ self_driving_car/network.js
@@ -2,9 +2,9 @@ class NeuralNetwork{
     constructor(neuronCounts){
         //creating network with levels from Level class
         this.levels=[];
-        for(let i=0; i<neuronCounts.lenght-1;i++){
+        for(let i=0;i<neuronCounts.length-1;i++){
             this.levels.push(new Level(
-                neuronCounts[i], neuronCounts[i+1]
+                neuronCounts[i],neuronCounts[i+1]
             ));
         }
     }
@@ -12,10 +12,10 @@ class NeuralNetwork{
     //returning outputs for them to be inputs for the next level
     static feedForward(givenInputs,network){
         let outputs=Level.feedForward(
-            givenInputs, network.levels[0]);
-        for(let i=1; i < network.levels.lenght;i++){
+            givenInputs,network.levels[0]);
+        for(let i=1;i<network.levels.length;i++){
             outputs=Level.feedForward(
-                outputs, network.levels[i]);
+                outputs,network.levels[i]);
         }
         return outputs;
     }
@@ -23,13 +23,13 @@ class NeuralNetwork{
 
 class Level{
     constructor(inputCount,outputCount){
-        this.inputs = new Array(inputCount);
-        this.outputs = new Array(outputCount);
-        this.biases = new Array(outputCount);
+        this.inputs=new Array(inputCount);
+        this.outputs=new Array(outputCount);
+        this.biases=new Array(outputCount);
 
-        this.weights = [];
-        for( let i=0; i<inputCount;i++){
-            this.weights[i] = new Array(outputCount);
+        this.weights=[];
+        for(let i=0;i<inputCount;i++){
+            this.weights[i]=new Array(outputCount);
         }
 
         Level.#randomize(this);
@@ -38,28 +38,29 @@ class Level{
 
     static #randomize(level){
         //initialize with random values between -1 and 1
-        for(let i=0; i<level.inputs.lenght; i++){
-            for(let j=0; j<level.outputs.lenght;j++){
+        for(let i=0;i<level.inputs.length;i++){
+            for(let j=0;j<level.outputs.length;j++){
                 level.weights[i][j]=Math.random()*2-1;
             }
         }
+
         // making negative to change direction in opposite direction
-        for(let i=0;i<level.biases.lenght;i++){
+        for(let i=0;i<level.biases.length;i++){
             level.biases[i]=Math.random()*2-1;
         }
     }
 
     static feedForward(givenInputs,level){
         // grabbing input values for given level
-        for(let i = 0; i<level.inputs.length; i++){
+        for(let i=0;i<level.inputs.length;i++){
             level.inputs[i]=givenInputs[i];
         }
 
-        // totaling values of inputs x weights of level inputs
-        for(let i=0; i<level.outputs.lenght;i++){
+        //totaling values of inputs x weight of level inputs
+        for(let i=0;i<level.outputs.length;i++){
             let sum=0
-            for(let j=0;j<level.inputs.lenght;j++){
-                sum+=level.inputs[j]*level.weights[j][i]
+            for(let j=0;j<level.inputs.length;j++){
+                sum+=level.inputs[j]*level.weights[j][i];
             }
 
             //triggers state to turn on if sum is greater than bias
@@ -67,7 +68,7 @@ class Level{
                 level.outputs[i]=1;
             }else{
                 level.outputs[i]=0;
-            }
+            } 
         }
 
         return level.outputs;
