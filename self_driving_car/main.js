0c8f2f45fe2e1b284fdcf255aa5db62b161ea0f4
Gonzalo Gamez <takosaga@gmail.com>
2023-12-29 13:00:47 +0200

    feat: able to save and trash best brain

--- self_driving_car/main.js
+++ self_driving_car/main.js
@@ -10,12 +10,26 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 
 const N = 100;
 const cars = generateCars(N);
+let bestCar= cars[0];
+if(localStorage.getItem("bestBrain")){
+    bestCar.brain=JSON.parse(
+        localStorage.getItem("bestBrain"));
+}
 
 const traffic = [
     new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2)
 ];
 
 animate();
+
+function save(){
+    localStorage.setItem("bestBrain",
+        JSON.stringify(bestCar.brain));
+}
+
+function discard(){
+    localStorage.removeItem("bestBrain");
+}
 
 function generateCars(N){
     const cars=[];
@@ -34,7 +48,7 @@ function animate(time) {
     }
     
 
-    const bestCar=cars.find(
+    bestCar=cars.find(
         c=>c.y==Math.min(
             //spreading an array of y values to find lowest
             ...cars.map(c=>c.y)
